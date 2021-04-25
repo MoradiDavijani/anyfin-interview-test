b04ed11c04b794fc8c3437fed5f8ae494561ac4a
Hossein Moradi <moradidavijani@gmail.com>
2021-04-25 17:31:05 +0430

    fix: migrate page components to typescript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
+import type { AppProps } from 'next/app'
 import Shell from 'src/shell'
 import 'src/styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Shell>
       <Component {...pageProps} />
