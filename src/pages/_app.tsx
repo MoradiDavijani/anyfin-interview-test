a5dc2ad134a5e54e8ffacdbbe2b86d61583c7556
Hossein Moradi <moradidavijani@gmail.com>
2021-04-26 04:39:26 +0430

    feat: add service worker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
+import React from 'react'
 import type { AppProps } from 'next/app'
 import { SettingsProvider } from 'src/stores/settings'
 import Shell from 'src/shell'
 import 'src/styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').then(
+        () => console.log('Service Worker registered successfully'),
+        error => console.log('Service Worker registration failed', error)
+      )
+    }
+  }, [])
+
   return (
     <SettingsProvider>
       <Shell>
